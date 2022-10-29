ff3176802545eced292118578de159360c656b9c
Lalit Gajanan Humane <102818469+lalithumane@users.noreply.github.com>
2022-10-29 06:37:24 +0530

    Add files via upload

---
+++ my_package/arithmetics.py
@@ -0,0 +1,22 @@
+def add_numbers(*nums):
+    total = 0
+    for i in nums:
+        total = total + i
+    
+    return total
+
+def subract(a,b):
+    return a - b
+
+
+def multiple(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
+
+def remainder(a,b):
+    return a % b
+
+def power(a,b):
+    return a ** b
---
+++ my_package/greet.py
@@ -0,0 +1,2 @@
+def freetings(first_name,last_name):
+    return f'{first_name} {last_name} this is my firs package creation and i am enjoying this  '
